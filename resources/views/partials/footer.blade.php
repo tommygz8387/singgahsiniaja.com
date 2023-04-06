20a340870e395ad099f3825fc04e1a3c7895bc28
tommygz8387 <tommy.gz.8387@gmail.com>
2023-04-06 15:01:04 +0700

    update 230406 --fix-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -36,8 +36,8 @@
     <div class="col-lg-2">
         <div class="footer_column">
             <ul class="footer_list footer_list_2">
-                <li><a href="#">Tiket Atraksi</a></li>
-                <li><a href="#">Paket Tour</a></li>
+                <li><a href="#">Open Trip</a></li>
+                <li><a href="#">Sewa Kendaraan</a></li>
             </ul>
         </div>
     </div>
@@ -46,9 +46,9 @@
         <div class="footer_column">
             <div class="footer_title">Customer Care</div>
             <ul class="footer_list">
-                <li><a href="#">My Account</a></li>
-                <li><a href="#">Order Tracking</a></li>
-                <li><a href="#">Wish List</a></li>
+                {{-- <li><a href="#">My Account</a></li>
+                <li><a href="#">Order Tracking</a></li> --}}
+                <li><a href="#">Admin</a></li>
                 <li><a href="#">Customer Services</a></li>
                 <li><a href="#">Returns / Exchange</a></li>
                 <li><a href="#">FAQs</a></li>
